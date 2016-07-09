8defeff09a57d490204ff99f720c8179e5455eff
Tona <trodriguez@drivercheck.ca>
2016-07-08 23:20:17 -0400

    Add simple hook for auth

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var app = express();
 
+app.use(function(req,res,next){
+	//we should handle autorization here for every request.
+	console.log("Handle auth");
+	if (true)
+		next();
+	else
+		res.send(401);
+});
+
 app.get('/', function (req, res) {
-   res.send('Hello World!! :D');
+	res.send('Hello World!! =D');
 })
 var port = process.env.PORT || 8000;
 var server = app.listen(port, function () {
